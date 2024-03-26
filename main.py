599057a1007f9eef0f12b737238b70acfc7de3dc
ruih8gta <nakagawashouta216@gmail.com>
2024-03-26 22:17:25 +0900

    second commit

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello World!!!'
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World Hey</p>"
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+if __name__ == "__main__":
+    app.run()
